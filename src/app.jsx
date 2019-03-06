102cd57b4eca58532f082d05cd54b07375bbc078
wwl6324 <411404821@qq.com>
2019-03-06 20:22:37 +0800

    first

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+import './index.css'
+import './index.scss'
+
+ReactDOM.render(
+    <div>
+        <h1>hello world</h1>
+    </div>,  
+    document.getElementById('root')
+)
